2d3ed38ed228b9db57f3075153d74b326816e98d
HaidilFitra <haidilfitra991@gmail.com>
2026-02-13 13:45:31 +0700

    Add controllers for categories, items, orders, roles, users, and dashboard; define resource routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MenuController;
-
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\DashboardController;
 Route::get('/', function () {
     return redirect()->route('menu');
 });
@@ -19,3 +24,14 @@ Route::get('cart/clear', [MenuController::class, 'clearCart'])->name('cart.clear
 Route::get('/checkout', [MenuController::class, 'checkout'])->name('checkout');
 Route::post('/checkout/store', [MenuController::class, 'storeOrder'])->name('checkout.store');
 Route::get('/checkout/success/{orderId}', [MenuController::class, 'checkoutSuccess'])->name('checkout.success');
+
+// Admin Routes
+// Route::get('/dashboard', DashboardController::class)->name('admin.dashboard');
+Route::get('/dashboard', function () {
+    return view('admin.dashboard');
+})->name('admin.dashboard');
+Route::resource('categories', CategoryController::class);
+Route::resource('items', ItemController::class);
+Route::resource('orders', OrderController::class);
+Route::resource('roles', RoleController::class);
+Route::resource('users', UserController::class);
